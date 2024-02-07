d42f7507f4d7db5aa54842c8056a49dab871903e
紅坂柚月 <akasaka@interpark.co.jp>
2024-02-07 16:24:42 +0900

    [update] v1.1.0

--- resource/script.js
+++ resource/script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', _ => {
   /**
    * バージョン
    */
-  document.getElementById('version').innerText = 'v1.0.1'
+  document.getElementById('version').innerText = 'v1.1.0'
 
   /**
    * エスケープ
