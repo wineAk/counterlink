35d5e603cd0a2312b3812eee86a7f60cc0e309bd
紅坂柚月 <akasaka@interpark.co.jp>
2024-02-13 09:13:01 +0900

    [update]

--- resource/script.js
+++ resource/script.js
@@ -7,9 +7,19 @@ window.addEventListener('load', _ => {
   /**
    * エスケープ
    */
-  const escape = str => {
+  const getEscapeTxt = str => {
     if (str == null) return null
-    const rep = str.replace(/</g, '&lt;').replace(/>/g, '&gt;').replace(/"/g, '&quot;').replace(/'/g, '&#39;').trim()
+    const rep = str
+      // XSS対策
+      .replace(/</g, '&lt;')
+      .replace(/>/g, '&gt;')
+      .replace(/"/g, '&quot;')
+      .replace(/'/g, '&#39;')
+      // カウンタ付リンク対策
+      .replace(/\[/g, '［')
+      .replace(/\]/g, '］')
+      .replace(/\|/g, '｜')
+      .trim()
     return rep
   }
 
@@ -28,19 +38,6 @@ window.addEventListener('load', _ => {
   }
 
   /**
-   * スコア値の制御
-   */
-  const changeScore = elm => {
-    elm.addEventListener('change', event => {
-      const value = event.target.value
-      const number = Number(value)
-      event.target.value = (number < -100) ? -100 : (number > 100) ? 100 : number
-    })
-  }
-  changeScore(document.querySelector('#scoreValue'))
-
-
-  /**
    * モーダルの処理
    */
   const imageModalElm = document.getElementById('imageModal')
@@ -52,46 +49,24 @@ window.addEventListener('load', _ => {
   })
 
   /**
+   * スコア値の制御
+   */
+  const changeScore = elm => {
+    elm.addEventListener('change', event => {
+      const value = event.target.value
+      const number = Number(value)
+      event.target.value = (number < -100) ? -100 : (number > 100) ? 100 : number
+    })
+  }
+  changeScore(document.querySelector('#scoreValue'))
+
+  /**
    * スコア一括
    */
   document.getElementById('scoreSet').addEventListener('click', event => {
     const value = document.getElementById('scoreValue').value
     document.querySelectorAll('[id^="counterlink-no-"] [type="number"]').forEach(e => e.value = value)
   })
-
-  /**
-   * 設定一覧のelementを返す
-   * @param {object} obj - alt, src, tooltip, error, text, index, checked, href
-   * @return {string} html
-   */
-  const getSettingHtmlAry = obj => {
-    const { alt, checked, error, href, index, src, text, tooltip } = obj
-    const imageButton = (alt == null) ? '' : `<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#imageModal" $data-bs-alt="${alt}" data-bs-src="${src}" data-bs-href="${href}" >画像</button>`
-    const HTML = `
-    <div class="col-sm-12 col-md-6 col-lg-4 col-xxl-3 mb-3" id="counterlink-no-${index}">
-      <div class="card h-100 bg-secondary">
-        <div class="card-header d-flex justify-content-between p-0">
-          <p class="m-0 py-2 px-3 text-truncate w-100" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="${tooltip}" data-bs-custom-class="${error}">${text}</p>
-          <input class="bg-transparent flex-shrink-0 form-check-input h2 m-0 rounded-0" type="checkbox" role="button" ${checked}>
-        </div>
-        <div class="card-body">
-          <a href="${href}" target="_blank" class="card-text link-dark text-break">${href}</a>
-        </div>
-        <div class="card-footer border-top-0 pt-0 pb-3 bg-transparent d-flex flex-wrap justify-content-between">
-          <div class="input-group mb-3">
-            <span class="input-group-text border-end-0">タイトル</span>
-            <textarea class="form-control" aria-label="${text}">${text}</textarea>
-          </div>
-          <div class="input-group w-75">
-            <span class="input-group-text border-end-0">スコア値</span>
-            <input type="number" class="form-control" id="counterlink-score-${index}"  min="-100" max="100" value="0">
-          </div>
-          ${imageButton}
-        </div>
-      </div>
-    </div>`
-    return HTML
-  }
 
   /**
    * ファイルの読み込み
@@ -117,37 +92,49 @@ window.addEventListener('load', _ => {
       const elmAry = Array.from(dom.querySelectorAll('a')).map((elm, index) => {
         console.log('elmAry - elm:', elm)
         // URL
-        const href = elm.href
+        const href = elm.getAttribute('href') || ''
         // インナーテキスト
-        const innerText = escape(elm.innerText)
+        const innerText = getEscapeTxt(elm.innerText)
         // 画像
         const imgElm = elm.querySelector('img')
-        const alt = escape(imgElm?.getAttribute('alt'))
+        const alt = getEscapeTxt(imgElm?.getAttribute('alt'))
         const src = imgElm?.getAttribute('src')
         // 双方なければ【テキストなし】
         const text = (alt) ? alt : (innerText) ? innerText : (src) ? '【テキストなし】' : ''
-
-        // 相対パス
-        const location = window.location
-        const origin = location.origin
-
-        // 相対パスの表示を作る
-        // ※このあたりはあとで作る
-        //const originAry = origin.split('/')
-        //const hrefAry = href.split('/')
-        //const tes = hrefAry.filter(v => !originAry.includes(v))
-
         // URLが相対パス、URLがない、タイトルがない、の場合はチェックしない
-        const isNotPath = (new RegExp(`^${origin}`).test(href))
-        const isNotHttp = (!isNotPath && !/^http/.test(href))
+        const isNotPath = (/^(\.+)?\//.test(href))//(new RegExp(`^${origin}`).test(href))
         const isNotURL = (!href.length)
+        const isNotHttp = (!/^http/.test(href))
         const isNotText = (!text.length)
         // ツールチップの内容
-        const tooltip = (isNotPath) ? '⚠ 相対パスです' : (isNotHttp) ? '⚠ URLではありません' : (isNotURL) ? '⚠ パスがありません' : (isNotText) ? '⚠ タイトルがありません' : text
-        const error = (/^⚠/.test(tooltip)) ? 'tooltip-danger' : 'tooltip-non'
-        const isChecked = (/^⚠/.test(tooltip)) ? '' : 'checked'
+        const tooltip = (isNotPath) ? '⚠ 相対パスです' : (isNotURL) ? '⚠ パスがありません' : (isNotHttp) ? '⚠ URLではありません' : (isNotText) ? '⚠ タイトルがありません' : text
+        const error = (tooltip != text) ? 'tooltip-danger' : 'tooltip-non'
+        const isChecked = (tooltip != text) ? '' : 'checked'
         const object = { alt: alt, src: src, tooltip: tooltip, error: error, text: text, index: index, checked: isChecked, href: href }
-        const HTML = getSettingHtmlAry(object)
+        const imageButton = (alt == null) ? '' : `<button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#imageModal" $data-bs-alt="${alt}" data-bs-src="${src}" data-bs-href="${href}" >画像</button>`
+        const HTML = `
+        <div class="col-sm-12 col-md-6 col-lg-4 col-xxl-3 mb-3" id="counterlink-no-${index}">
+          <div class="card h-100 bg-secondary">
+            <div class="card-header d-flex justify-content-between p-0">
+              <p class="m-0 py-2 px-3 text-truncate w-100" data-bs-toggle="tooltip" data-bs-placement="top" data-bs-title="${tooltip}" data-bs-custom-class="${error}">${text}</p>
+              <input class="bg-transparent flex-shrink-0 form-check-input h2 m-0 rounded-0" type="checkbox" role="button" ${isChecked}>
+            </div>
+            <div class="card-body">
+              <a href="${href}" target="_blank" class="card-text link-dark text-break">${href}</a>
+            </div>
+            <div class="card-footer border-top-0 pt-0 pb-3 bg-transparent d-flex flex-wrap justify-content-between">
+              <div class="input-group mb-3">
+                <span class="input-group-text border-end-0">タイトル</span>
+                <textarea class="form-control" aria-label="${text}">${text}</textarea>
+              </div>
+              <div class="input-group w-75">
+                <span class="input-group-text border-end-0">スコア値</span>
+                <input type="number" class="form-control" id="counterlink-score-${index}"  min="-100" max="100" value="0">
+              </div>
+              ${imageButton}
+            </div>
+          </div>
+        </div>`
         return HTML
       })
       if (!elmAry.length) {
@@ -179,7 +166,7 @@ window.addEventListener('load', _ => {
    */
   document.getElementById('download').addEventListener('click', event => {
     const checkAry = Array.from(document.querySelectorAll('[id^="counterlink-no-"] [type="checkbox"]')).map(elm => elm.checked)
-    const titleAry = Array.from(document.querySelectorAll('[id^="counterlink-no-"] textarea')).map(elm => escape(elm.value.replace(/[\[:|\]]/g, '')))
+    const titleAry = Array.from(document.querySelectorAll('[id^="counterlink-no-"] textarea')).map(elm => getEscapeTxt(elm.value))
     const scoreAry = Array.from(document.querySelectorAll('[id^="counterlink-no-"] [type="number"]')).map(elm => elm.value)
     // 保存したDOMをコピー
     const dom = htmlFileDom.getElementsByTagName('html')[0].cloneNode(true)
