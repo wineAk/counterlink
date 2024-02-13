be6614982ab066ca5a2700046ec9f74a376ef17f
紅坂柚月 <akasaka@interpark.co.jp>
2024-02-13 14:40:03 +0900

    [update] v1.4.1

--- resource/script.js
+++ resource/script.js
@@ -2,7 +2,7 @@ window.addEventListener('load', _ => {
   /**
    * バージョン
    */
-  document.getElementById('version').innerText = '1.4.0'
+  document.getElementById('version').innerText = '1.4.1'
 
   /**
    * エスケープ
